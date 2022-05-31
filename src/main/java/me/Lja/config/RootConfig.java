8fc386071f9d53f4eafa0e86e642c55367e57885
redgun123 <red_gun@naver.com>
2022-05-31 12:35:36 +0900

    sql커넥션

--- src/main/java/me/Lja/config/RootConfig.java
+++ src/main/java/me/Lja/config/RootConfig.java
@@ -5,23 +5,41 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @MapperScan("me.Lja.mapper")
+@PropertySources({
+	@PropertySource("classpath:datasource/jdbc.properties")	
+})
 public class RootConfig {
+	
+	@Value("${jdbc.dirverClassName}")
+	private String driverClassName;
+	
+	@Value("${jdbc.url}")
+	private String url;
+	
+	@Value("${jdbc.username}")
+	private String username;
+	
+	@Value("${jdbc.password}")
+	private String password;
 	
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig config = new HikariConfig();
-		config.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
-		config.setJdbcUrl("jdbc:log4jdbc:mysql://localhost/board.ex");
-		config.setUsername("root");
-		config.setPassword("1234");
+		config.setDriverClassName(driverClassName);
+		config.setJdbcUrl(url);
+		config.setUsername(username);
+		config.setPassword(password);
 		HikariDataSource dataSource = new HikariDataSource(config); 
 		return dataSource;
 	}
